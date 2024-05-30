9ffd6c8b23f0050e08ce3d65c123fb56df0fd925
Samson <samsonsurin22@gmail.com>
2024-05-30 13:31:44 +0530

    updated

--- src/components/ImageSlidshow.jsx
+++ src/components/ImageSlidshow.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { ImagesSlider } from './ui/imageslider';
 import { motion } from 'framer-motion';
 import { getImageURL } from '../utils/image-util';
+import { Link } from 'react-router-dom';
 
 const ImageSlidshow = () =>{
   const images = Array.from({ length: 17 }, (_, i) => (getImageURL(`past/h${i + 1}.webp`)));
-  console.log(images);
   return (
     <>
     <h2 className="font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
@@ -26,11 +26,12 @@ const ImageSlidshow = () =>{
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        
+        <Link to="/about">
         <button className="px-2 py-2 my-2 md:px-4 md:py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full absolute bottom-5 ">
           <span>Know More →</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
+        </Link>
       </motion.div>
     </ImagesSlider>
     </>
