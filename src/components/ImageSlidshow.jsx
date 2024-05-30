814a72c82f4e3c08b280e0e71bf804e93e379ee4
Samson <samsonsurin22@gmail.com>
2024-05-30 19:13:13 +0530

    changed button

--- src/components/ImageSlidshow.jsx
+++ src/components/ImageSlidshow.jsx
@@ -4,36 +4,59 @@ import { motion } from 'framer-motion';
 import { getImageURL } from '../utils/image-util';
 import { Link } from 'react-router-dom';
 
-const ImageSlidshow = () =>{
+const ImageSlidshow = () => {
   const images = Array.from({ length: 17 }, (_, i) => (getImageURL(`past/h${i + 1}.webp`)));
   return (
     <>
-    <h2 className="font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
+      <h2 className="font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
         A Glimpse Into The Past
       </h2>
-    <ImagesSlider id='imageSlider' className="h-[30vh] md:h-[35rem] w-[80vw] mx-auto " images={images}>
-      <motion.div
-        initial={{
-          opacity: 0,
-          y: -80,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          duration: 0.6,
-        }}
-        className="z-50 flex flex-col justify-center items-center"
-      >
-        <Link to="/about">
-        <button className="px-2 py-2 my-2 md:px-4 md:py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full absolute bottom-5 ">
-          <span>Know More →</span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+      <ImagesSlider id='imageSlider' className="h-[30vh] md:h-[35rem] w-[80vw] mx-auto " images={images}>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: -80,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.6,
+          }}
+          className="z-50 flex flex-col justify-center items-center"
+        >
+
+        </motion.div>
+      </ImagesSlider>
+      <Link to="/about">
+        <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block my-2">
+          <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+          </span>
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+            <span className='py-3 text-sm'>
+              Know More
+            </span>
+            <svg
+              fill="none"
+              height="16"
+              viewBox="0 0 24 24"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.75 8.75L14.25 12L10.75 15.25"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+              />
+            </svg>
+          </div>
+          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
         </button>
-        </Link>
-      </motion.div>
-    </ImagesSlider>
+      </Link>
     </>
   );
 }
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,31 +5,55 @@ import space from '../assets/images/space-lost.png'
 const PageNotFound = () => {
   return (
     <>
-<div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-            <div className="xl:pt-24 w-full xl:w-1/2 pb-12 lg:pb-0">
-                <div className="relative">
-                    <div className="absolute">
-                            <h1 className="my-2 text-gray-100 font-bold text-2xl">
-                                Looks like you've found the
-                                doorway to the great nothing
-                            </h1>
-                            <p className="my-2 text-gray-100">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                           
-                    </div>
-                    <div>
-                        <img className='opacity-30' src="https://i.ibb.co/G9DC8S0/404-2.png" />
-                        <a href="/">
-                            <button className="z-10 w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-800 text-white hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                              Take me there!</button>
-                            </a>
-                    </div>
-                </div>
+      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+        <div className="xl:pt-24 w-full xl:w-1/2 pb-12 lg:pb-0">
+          <div className="relative">
+            <div className="absolute">
+              <h1 className="my-2 text-gray-100 font-bold text-2xl">
+                Looks like you've found the
+                doorway to the great nothing
+              </h1>
+              <p className="my-2 text-gray-100">Sorry about that! Please visit our hompage to get where you need to go.</p>
+
             </div>
             <div>
-                <img src={space} className='w-52 mt-2'/>
+              <img className='opacity-30' src="https://i.ibb.co/G9DC8S0/404-2.png" />
+              <a href="/">
+                <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block my-20">
+                  <span className="absolute inset-0 overflow-hidden rounded-full">
+                    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                  </span>
+                  <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                    <span className='py-3 text-sm'>
+                      Take Me There
+                    </span>
+                    <svg
+                      fill="none"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      width="16"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10.75 8.75L14.25 12L10.75 15.25"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.5"
+                      />
+                    </svg>
+                  </div>
+                  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                </button>
+              </a>
             </div>
+          </div>
         </div>
-</>
+        <div>
+          <img src={space} className='w-52 mt-2' />
+        </div>
+      </div>
+    </>
   )
 }
 
