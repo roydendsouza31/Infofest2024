243c290947adcb7afaa51d62f325130567fac3bd
Samson <samsonsurin22@gmail.com>
2024-06-01 21:37:47 +0530

    updated

--- src/components/FeaturedEventsGrid.jsx
+++ src/components/FeaturedEventsGrid.jsx
@@ -18,15 +18,15 @@ export default function FeaturedEventsGrid() {
 
   const translateY = useTransform(scrollYProgress, [0, 1], [10, 450]);
 
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    // console.log("Page scroll: ", scrollYProgress)
-    console.log(latest)
-  })
   return (
+    <div className='relative bg-fixed bg-cover py-5 md:py-10' style={{backgroundImage:`url('https://img.freepik.com/free-photo/night-sky-with-planets-galaxies-scene-generative-ai_188544-7873.jpg?t=st=1717138646~exp=1717142246~hmac=a6d7ca4d177451d38e614e2194ce2c3573b7ee9cecc688c6a1baa033497fe2c5&w=826')`}}>
+
+  <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
     <motion.div ref={ref} variants={fadeIn("down", 0.1)}
     initial="hidden"
     whileInView={"show"}
     viewport={{ once: false, ammount: 0.7 }} >
+      
       <h2 className="font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
         Events
       </h2>
@@ -82,7 +82,7 @@ export default function FeaturedEventsGrid() {
       </motion.div>
 
 
-      <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block">
+      <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block z-0">
         <span className="absolute inset-0 overflow-hidden rounded-full">
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
         </span>
@@ -109,6 +109,7 @@ export default function FeaturedEventsGrid() {
         <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
       </button>
     </motion.div>
+    </div>
   )
 }
 
