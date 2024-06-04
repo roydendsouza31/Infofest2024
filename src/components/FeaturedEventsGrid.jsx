3676596a0d7bcc1b1229b27bd3dea33339679823
Samson <samsonsurin22@gmail.com>
2024-06-04 22:01:30 +0530

    added audiowide , orbitron and chakra fonts

--- src/components/FeaturedEventsGrid.jsx
+++ src/components/FeaturedEventsGrid.jsx
@@ -27,7 +27,7 @@ export default function FeaturedEventsGrid() {
     whileInView={"show"}
     viewport={{ once: false, ammount: 0.7 }} >
       
-      <h2 className="font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
+      <h2 className="font-audiowide font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
         Events
       </h2>
       <motion.div className='grid grid-cols-1 md:grid-cols-3 px-10  md:px-20 my-5'>
@@ -87,7 +87,7 @@ export default function FeaturedEventsGrid() {
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
         </span>
         <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-          <span className='py-3 text-sm'>
+          <span className='py-3 text-sm font-orbitron'>
             More Events
           </span>
           <svg
@@ -106,7 +106,7 @@ export default function FeaturedEventsGrid() {
             />
           </svg>
         </div>
-        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+        <span className="absolute  -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
       </button>
     </motion.div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
 
   // Random component
-  const Completionist = () => <span>You are good to go!</span>;
+  const Completionist = () => <span>Live</span>;
 
 
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
@@ -31,7 +31,7 @@ const Hero = () => {
         <>
           <h2 className='text-center my-2'>Remaing Time</h2>
           {/* <hr className='my-2'/> */}
-          <div className='countdown font-normal flex space-x-2 md:space-x-5'>
+          <div className='countdown text-sm md:text-2xl flex space-x-2 md:space-x-5'>
             <div className='text-center'><div>{days}</div>
               <div>Days</div></div>
             <span>:</span>
@@ -51,7 +51,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="relative overflow-hidden min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+    <div className="relative overflow-hidden font-audiowide min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
       <img className="absolute inset-0 w-full h-full object-cover" src={bg} alt="background" />
       <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
 
@@ -75,7 +75,7 @@ const Hero = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 2 }}
           className="md:-mt-16 ">
-          <span className="text-sm md:text-base font-semibold">In Association with</span>
+          <span className="text-sm md:text-base">In Association with</span>
           <div className="flex space-x-4 justify-center mt-2">
             <img width="35px" src={ntep} alt="National Tuberculosis Elimination Program" title="National Tuberculosis Elimination Program" />
             <img width="35px" src={betibacho} alt="Beti Bachao Beti Padhao" title="Beti Bachao Beti Padhao" />
@@ -86,7 +86,7 @@ const Hero = () => {
 
         <motion.div
           transition={1}
-          className="mt-6 text-2xl font-semibold transition-all ease-in-out">
+          className="mt-6 text-2xl  transition-all ease-in-out">
           {/* Remaining Time: <span className='animate-spin'>{countdown?countdown: <Typewriter
             words={['.......................']}
             loop={0}
@@ -105,7 +105,7 @@ const Hero = () => {
           transition={{ duration: 1 }}
           className='my-5'
         >
-          <Button borderRadius="2.75rem" className="font-semibold h-12 bg-[rgb(15 23 42 / 0%)]" containerClassName="h-15 w-max">
+          <Button borderRadius="2.75rem" className="h-12 bg-[rgb(15 23 42 / 0%)]" containerClassName="h-15 w-max">
             <a href="" className='px-5'>Register Now</a>
           </Button>
         </motion.div>
