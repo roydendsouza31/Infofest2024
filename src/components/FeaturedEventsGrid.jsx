fb6d17f50c9bbd48204041fff6f4c11ea83b757f
Samson <samsonsurin22@gmail.com>
2024-09-10 21:11:56 +0530

    updated featured events

--- src/components/FeaturedEventsGrid.jsx
+++ src/components/FeaturedEventsGrid.jsx
@@ -13,231 +13,134 @@ import rocket from '../assets/lottie/rocket.json'
 
 export default function FeaturedEventsGrid() {
   const ref = useRef(null);
+
+  const featuredEvents = [
+    {
+      event: 'Hackthon',
+      image: 'Hackthon.webp',
+    },
+    {
+      event: 'CTF',
+      image: 'CTF.webp',
+    },
+    {
+      event: 'Level Up Coding',
+      image: 'LevelUpCoding.webp',
+    },
+    {
+      event: 'Line Follower',
+      image: 'LineFollower.webp',
+    },
+    {
+      event: 'UI UX',
+      image: 'UIUX.webp',
+    },
+    {
+      event: 'Tech Debate',
+      image: 'techdebate.webp',
+    },
+  ]
+
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 0.6", "end end"]
   });
 
- 
-  const translateY = useTransform(scrollYProgress, [0, 1], [10, 450]);
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [10, 600]);
   const style = {
     height: 200,
     transform: 'rotate(180deg)'
   };
   return (
-    <div className='relative bg-fixed bg-cover py-5 md:py-10' style={{backgroundImage:`url(${getImageURL('eventbg.jpg')})`}}>
+    <div className='relative bg-fixed bg-cover py-5 md:py-10' style={{ backgroundImage: `url(${getImageURL('eventbg.jpg')})` }}>
 
-  <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
-    <motion.div ref={ref} variants={fadeIn("down", 0.1)}
-    initial="hidden"
-    whileInView={"show"}
-    viewport={{ once: false, ammount: 0.7 }} >
-      
-      <h2 className="font-audiowide font-bold text-2xl md:text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 my-5 mt-10">
-        Events
-      </h2>
-      <motion.div className='grid grid-cols-1 md:grid-cols-3 px-10  md:px-20 my-5'>
-        <div className='space-y-10 my-10 md:my-10'>
-          <motion.div
-            variants={fadeIn("left", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
-          <motion.div
-            variants={fadeIn("left", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
-          <motion.div
-            variants={fadeIn("left", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
+      {/* <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div> */}
+      <motion.div ref={ref} variants={fadeIn("down", 0.1)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, ammount: 0.7 }} >
 
+        <h2 className="font-audiowide font-bold text-white text-2xl md:text-3xl text-center bg-clip-text my-5 mt-10">
+          Events
+        </h2>
+        <motion.div className='grid grid-cols-1 md:grid-cols-3 px-10  md:px-20 my-5 font-audiowide text-2xl'>
+          {/* LEFT SECTION */}
+          <div className=' '>
+            {featuredEvents.slice(0, 3).map(e => Card(e,"left")
+            )}
 
-        </div>
-        <motion.div 
-          initial={{ opacity: 0 }}
-          animate={{ opacity:1  }}
-          transition={{ duration: 5 }} className='hidden md:block relative mx-auto'>
-          {/* <motion.img src={getImageURL('rocket.svg')} style={{ translateY: translateY }} alt="rocket" /> */}
-          <motion.div  style={{ translateY: translateY}} >
-          <Lottie animationData={rocket} style={style} />;
+          </div>
+
+          {/* ROCKET */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 5 }} className='hidden md:block relative mx-auto'>
+            {/* <motion.img src={getImageURL('rocket.svg')} style={{ translateY: translateY }} alt="rocket" /> */}
+            <motion.div style={{ translateY: translateY }} >
+              <Lottie animationData={rocket} style={style} />
+            </motion.div>
           </motion.div>
+
+          {/* RIGHT SECTION  */}
+          <div className=''>
+            {featuredEvents.slice(3, 7).map(e => Card(e,"right")
+            )}
+          </div>
         </motion.div>
-        <div className='space-y-10 my-10 md:my-10'>
-          <motion.div
-            variants={fadeIn("right", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
-          <motion.div
-            variants={fadeIn("right", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
-          <motion.div
-            variants={fadeIn("right", 0.1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, ammount: 0.7 }}
-            className="card min-h-40 min-w-5 border-white border-2 rounded-2xl"></motion.div>
-        </div>
-      </motion.div>
 
 
-      <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block z-0">
-        <span className="absolute inset-0 overflow-hidden rounded-full">
-          <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-        </span>
-        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-          <span className='py-3 text-sm font-orbitron'>
-            More Events
+        <button className="bg-slate-800 mx-auto  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block z-0">
+          <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
           </span>
-          <svg
-            fill="none"
-            height="16"
-            viewBox="0 0 24 24"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.75 8.75L14.25 12L10.75 15.25"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1.5"
-            />
-          </svg>
-        </div>
-        <span className="absolute  -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-      </button>
-    </motion.div>
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+            <span className='py-3 text-sm font-orbitron'>
+              More Events
+            </span>
+            <svg
+              fill="none"
+              height="16"
+              viewBox="0 0 24 24"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.75 8.75L14.25 12L10.75 15.25"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+              />
+            </svg>
+          </div>
+          <span className="absolute  -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+        </button>
+      </motion.div>
     </div>
   )
 }
 
 
-const Card = ({ event, index }) => {
+const Card = (event,direction) => {
   return (
-    <Link to={`/events/${event.slug}`}>
-      <StyledCard $bg={event.image}>
-        <div className="bg-overlay"></div>
-        <div className="title">
-          <h3><span>{(index == 0) ? event.title : event.codeName.toUpperCase()}</span></h3>
-          <p>{(index == 0) ? event.description : event.title}</p>
+    <Link to={`/events/${event.event}`}>
+      <motion.div
+        variants={fadeIn(direction, 0.1)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, ammount: 0.7 }}
+        className="card min-h-40 h-56 min-w-5 border-2 border-orange-100 rounded-2xl bg-cover mb-3 md:mb-8 animate-vflip"
+        style={{ backgroundImage: `url(${getImageURL(`events/${event.image}`)})` }}>
+        <div className='text-white'>
+          <div className="px-10 my-[40%]">
+            {event.event}
+          </div>
         </div>
-        <div className="meta">
-          {/* <span className="date">{event.date.replaceAll('-', '.')}</span> */}
-          <button className="more-button"><span>more</span></button>
-        </div>
-      </StyledCard>
+      </motion.div>
     </Link>
   )
 }
 
-const StyledCard = styled.div`
-  align-items: flex-end;
-  border: 1px solid #979797;
-  box-sizing: border-box;
-  color: #cdcdcd;
-  display: flex;
-  height: 100%;
-  justify-content: space-between;
-  overflow: hidden;
-  padding: 0 30px 30px;
-  position: relative;
-  text-decoration: none;
-  background: url('${props => props.$bg}') no-repeat;
-  background-size: cover;
-  position: relative;
 
-  .bg-overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(0deg, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.5) 100% );
-  }
-
-  .title, .meta {
-    z-index: 1;
-  }
-
-  & .title { padding-top: 100px; }
-  & p { font-family: BlenderProBold,sans-serif }
-
-  & .meta {
-    position: absolute;
-    right: 24px;
-    top: 44px;
-  }
-
-  & .more-button, & .date {
-    color: #cdcdcd;
-    display: block;
-    font-family: BlenderProBold,sans-serif;
-    font-size: 14px;
-    font-weight: 700;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    border-style: none;
-    background: none;
-  }
-
-  & .more-button { display: none;  cursor: pointer; }
-
-  &:hover {
-    background: #cdcdcd;
-    color: #000;
-    * { color: #000 !important; }
-    .date { display: none; }
-    .more-button { display: block; }
-
-    &::after { background: #000 }
-
-    .bg-overlay { opacity: 0 }
-  }
-
-  &::after {
-    background: #cdcdcd;
-    content: "";
-    display: block;
-    height: 3px;
-    left: 29px;
-    position: absolute;
-    top: 52px;
-    width: 53px;
-  }
-
-  @media screen and (max-width: 767px) {
-    &.title { padding-top: 50px; }
-    & .meta { top: 19px; }
-    &::after { top: 26px; }
-  }
-`
-
-const Container = styled.div`
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  margin-top: 5px;
-  
-  a {
-    color: #cdcdcd;
-    padding: 10px 20px;
-    border: 1px solid #979797;
-
-    &:hover {
-      background: #cdcdcd;
-      color: #000;
-    }
-  }
-
-`
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,56 +1,8 @@
-import React, { Suspense, useRef, useEffect } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Earth } from '../../public/Earth.jsx'
-import '../earth.css'
-import * as THREE from 'three'
-
-const StarField = () => {
-  const groupRef = useRef()
-  const starMeshRef = useRef()
-
-  useEffect(() => {
-    const group = groupRef.current
-    const stars = new THREE.BufferGeometry()
-    const starCount = 5000
-
-    const positions = []
-    for (let i = 0; i < starCount; i++) {
-      const x = THREE.MathUtils.randFloatSpread(2000)
-      const y = THREE.MathUtils.randFloatSpread(2000)
-      const z = THREE.MathUtils.randFloatSpread(2000)
-      positions.push(x, y, z)
-    }
-
-    stars.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
-
-    const starMaterial = new THREE.PointsMaterial({ color: 0xffffff })
-
-    const starMesh = new THREE.Points(stars, starMaterial)
-    starMeshRef.current = starMesh
-    group.add(starMesh)
-  }, [])
-
-  useFrame(() => {
-    if (starMeshRef.current) {
-      starMeshRef.current.rotation.y += 0.0005 // Rotate stars slowly around the y-axis
-    }
-  })
-
-  return <group ref={groupRef} />
-}
+import React from 'react'
 
 const Event = () => {
   return (
-      <Canvas style={{ background: 'black' }}>
-        <ambientLight intensity={2}/>
-        <OrbitControls enableZoom={false}/>
-        <Suspense fallback={null}>
-          {/* <Earth /> */}
-        </Suspense>
-        {/* <Environment preset='sunset'/> */}
-        <StarField />
-      </Canvas>
+    <div>Event</div>
   )
 }
 
