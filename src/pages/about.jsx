11fad7384665fb3d9e17ace47dfaa1bb91aab2c7
Samson <samsonsurin22@gmail.com>
2024-10-07 17:06:56 +0530

    updated about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { getImageURL } from '../utils/image-util';
 
 const About = () => {
   return (
-    <div className=" mt-10 min-h-screen flex flex-col items-center justify-center p-6">
-      <div className="bg-gray-800 shadow-md rounded-lg p-8 max-w-3xl">
-        <h1 className="text-4xl font-bold mb-6 text-center text-yellow-400">About Infofest</h1>
+    <div className="mt-20 min-h-screen flex flex-col items-center  justify-center p-2 md:p-6">
+      <div className="border-2 border-orange-500 shadow-md w-full md:w-[90%] rounded-lg p-3 md:p-8  font-audiowide">
+        <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center text-yellow-400 font-orbitron">About Infofest</h1>
+        <figure>
+          <img src={getImageURL('if24shortlogo.svg')} alt="if24shortlogo" className='mx-auto w-52 md:w-72 h-auto'/>
+        </figure>
         <p className="text-lg text-gray-300 leading-relaxed mb-4">
           Welcome to <span className="font-semibold text-yellow-400">Infofest</span>, the ultimate coding competition for students! Our mission is to bring together young, talented programmers from around the nation to showcase their skills, learn from industry experts, and compete for exciting prizes.
         </p>
